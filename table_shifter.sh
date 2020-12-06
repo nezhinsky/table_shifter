dbdeb5dafdf8a77870b66f83511b61fea5a9d020
Alon Carmelly <alon212@gmail.com>
2020-12-06 14:49:15 +0200

    Update main condition for min args input

--- table_shifter.sh
+++ table_shifter.sh
@@ -1,48 +1,32 @@
 #!/usr/bin/bash
 
-if [[ $# < 3 ]]; then
-    echo "not enough args"
-    exit 1
+if [[ $# < 3 ]]; then 
+  echo "insuffianct Args"
+  exit 1
 fi
 
-if [ -z "$2" ]; then
-   echo "Please enter colume"
-   else
-    readarray -t HEADER <$1
-    #for loop to enumarte the selected colume on $2
-    arr_pos=0
-    for col in $HEADER;
-    do
-    if [ $col = $2 ]
-    then
-    pos1="$arr_pos"
-    break
-    else
-    ((arr_pos++))
-    fi
-    done
+readarray -t HEADER <$1
+#for loop to enumarte the selected colume on $2
+arr_pos=0
+for col in $HEADER;
+do
+if [ $col = $2 ]
+then
+pos1="$arr_pos"
+break
+else
+((arr_pos++))
 fi
-
-
+done
 #echo $pos1
 #---------------------------------------------
 #itirates through the file and if third conditon is met the #prints the requested line and colum
 
-
-
-if [ -z "$3" ]; then
-   echo "Please input a colume filter"
-   else
-   while IFS=' ' read -r line
-   do
-   arr=($line)
-   if [[ ${arr[$pos1]} =~ K$3 ]]
-   then
-   echo "${arr[@]}"
-   fi
-   done < $1
-
-
-fi
-
-
+while IFS=' ' read -r line
+do
+  arr=($line)
+  if [[ ${arr[$pos1]} =~ $3 ]]
+  then
+  echo "${arr[@]}"
+  fi
+done < $1
