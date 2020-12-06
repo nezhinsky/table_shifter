7bf7c64c74d01f0fb7369f9c9c9e740e8de60f94
Alon Carmelly <alon212@gmail.com>
2020-12-06 09:55:11 +0200

    check if input exists for  and

--- table_shifter.sh
+++ table_shifter.sh
@@ -1,26 +1,43 @@
 #!/usr/bin/bash
-readarray -t HEADER <$1
-#for loop to enumarte the selected colume on $2
-arr_pos=0
-for col in $HEADER;
-do
-if [ $col = $2 ]
-then
-pos1="$arr_pos"
-break
-else
-((arr_pos++))
+
+if [ -z "$2" ]; then
+   echo "Please enter colume"
+   else
+    readarray -t HEADER <$1
+    #for loop to enumarte the selected colume on $2
+    arr_pos=0
+    for col in $HEADER;
+    do
+    if [ $col = $2 ]
+    then
+    pos1="$arr_pos"
+    break
+    else
+    ((arr_pos++))
+    fi
+    done
 fi
-done
+
+
 #echo $pos1
 #---------------------------------------------
 #itirates through the file and if third conditon is met the #prints the requested line and colum
 
-while IFS=' ' read -r line
-do
-  arr=($line)
-  if [[ ${arr[$pos1]} =~ $3 ]]
-  then
-  echo "${arr[@]}"
-  fi
-done < $1
+
+
+if [ -z "$3" ]; then
+   echo "Please input a colume filter"
+   else
+   while IFS=' ' read -r line
+   do
+   arr=($line)
+   if [[ ${arr[$pos1]} =~ $3 ]]
+   then
+   echo "${arr[@]}"
+   fi
+   done < $1
+
+
+fi
+
+
