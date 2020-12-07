2e4bc785ab5bc2d17646445b26db44963213273f
Alon Carmelly <alon212@gmail.com>
2020-12-07 16:00:46 +0200

    bug fix for table_shifter as per our session

--- table_shifter.sh
+++ table_shifter.sh
@@ -1,28 +1,35 @@
 #!/usr/bin/bash
 
-err_msg () {
-echo "Insufficient arguments"
+function usage () {
+echo "NOTE: ./table_shifter <file> <column name> <column val>"
 }
 
 
 if [[ $# < 3 ]]; then 
-  err_msg
+  echo "Insufficient arguments expected 3 you enterd $# "
+  usage
   exit 1
 fi
 
-readarray -t HEADER <$1
+readarray -n 1 -t HEADER <$1
 #for loop to enumarte the selected colume on $2
 arr_pos=0
 for col in $HEADER;
-do
-if [[ $col == $2 ]]
-then
-pos1="$arr_pos"
-break
-else
-((arr_pos++))
-fi
+  do
+    if [[ "$col" == "$2" ]]
+    then
+    pos1="$arr_pos"
+    break
+    else
+    ((arr_pos++))
+    fi
 done
+if [ -z $pos1 ];
+  then
+  echo "ERROR : column name does not exist"
+  usage
+  exit 1
+fi
 #echo $pos1
 #---------------------------------------------
 #itirates through the file and if third conditon is met the #prints the requested line and colum
